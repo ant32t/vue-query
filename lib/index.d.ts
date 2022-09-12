a8101f21fb384e1192c3951260735a5982826840
Philip Reimer <philip@teleoinc.com>
2022-09-12 13:21:34 -0600

    feat: add lib

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+import { QueryCache, MutationCache, QueryClient, MutationFilters, QueryFilters } from "./vuejs";
+export * from "@tanstack/query-core";
+export * from "./vuejs";
+export { QueryCache, MutationCache, QueryClient, MutationFilters, QueryFilters, };
